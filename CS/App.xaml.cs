c881b094cdab4373c9fab6845144a1371bc5c954
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿// Developer Express Code Central Example:
+// How to manually implement the Auto Filter Row feature
+// 
+// The following example demonstrates how to create a Filter Control to filter data
+// within DXGrid, so that only data rows that meet the current row filter are
+// visible. If you want to be notified when this feature is implemented for the
+// DXGrid control, please start tracking the corresponding suggestion: .
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E954
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace MultiValueConverter {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
