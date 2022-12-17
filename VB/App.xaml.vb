05238140c406fd73248438638e250c7670ca9086
DevExpressExampleBot <fakeEmail>
2022-12-17 13:09:53 +0400

    Source auto update [skip ci]

--- VB/App.xaml.vb
+++ VB/App.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to manually implement the Auto Filter Row feature
 ' 
 ' The following example demonstrates how to create a Filter Control to filter data
@@ -8,20 +8,15 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E954
-
-Imports System
-Imports System.Collections.Generic
-Imports System.Configuration
-Imports System.Data
-Imports System.Linq
 Imports System.Windows
 
 Namespace MultiValueConverter
-	''' <summary>
-	''' Interaction logic for App.xaml
-	''' </summary>
-	Partial Public Class App
-		Inherits Application
 
-	End Class
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
 End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,69 +1,54 @@
-﻿Imports System
+Imports System
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Collections.Generic
-Imports DevExpress.Data.Filtering
 Imports System.ComponentModel
-Imports System.Windows.Input
 Imports System.Globalization
 Imports System.Windows.Markup
-Imports System.Threading
 Imports System.Windows.Data
 
 Namespace MultiValueConverter
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Private list As BindingList(Of Order)
+        Private list As BindingList(Of Order)
 
-		Public Sub New()
-			InitializeComponent()
+        Public Sub New()
+            Me.InitializeComponent()
+            list = New BindingList(Of Order)()
+            list.Add(New Order() With {.Item = "Salad", .Price = 6.99D, .Quantity = 1})
+            list.Add(New Order() With {.Item = "Cheeseburger", .Price = 2.75D, .Quantity = 2})
+            list.Add(New Order() With {.Item = "Cola", .Price = 1.50D, .Quantity = 1})
+            Me.grid1.ItemsSource = list
+        End Sub
+    End Class
 
-			list = New BindingList(Of Order)()
-			list.Add(New Order() With {
-				.Item = "Salad",
-				.Price = 6.99D,
-				.Quantity = 1
-			})
-			list.Add(New Order() With {
-				.Item = "Cheeseburger",
-				.Price = 2.75D,
-				.Quantity = 2
-			})
-			list.Add(New Order() With {
-				.Item = "Cola",
-				.Price = 1.50D,
-				.Quantity = 1
-			})
+    Public Class MyConverter
+        Inherits MarkupExtension
+        Implements IMultiValueConverter
 
-			grid1.ItemsSource = list
-		End Sub
-	End Class
-	Public Class MyConverter
-		Inherits MarkupExtension
-		Implements IMultiValueConverter
+'#Region "IValueConverter Members"
+        Private Function Convert(ByVal values As Object(), ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IMultiValueConverter.Convert
+            If values(0) Is DependencyProperty.UnsetValue OrElse values(1) Is DependencyProperty.UnsetValue Then Return Decimal.Zero
+            Return System.Convert.ToDecimal(values(0)) * System.Convert.ToInt32(values(1))
+        End Function
 
-		#Region "IValueConverter Members"
-		Private Function IMultiValueConverter_Convert(ByVal values() As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IMultiValueConverter.Convert
-			If values(0) Is DependencyProperty.UnsetValue OrElse values(1) Is DependencyProperty.UnsetValue Then
-				Return Decimal.Zero
-			End If
-			Return Convert.ToDecimal(values(0)) * Convert.ToInt32(values(1))
-		End Function
-		Private Function IMultiValueConverter_ConvertBack(ByVal value As Object, ByVal targetTypes() As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
-			Throw New NotImplementedException()
-		End Function
-		#End Region
+        Private Function ConvertBack(ByVal value As Object, ByVal targetTypes As Type(), ByVal parameter As Object, ByVal culture As CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
+            Throw New NotImplementedException()
+        End Function
 
-		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-			Return Me
-		End Function
-	End Class
+'#End Region
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return Me
+        End Function
+    End Class
 
-	Public Class Order
-		Public Property Item() As String
-		Public Property Price() As Decimal
-		Public Property Quantity() As Integer
-	End Class
+    Public Class Order
+
+        Public Property Item As String
+
+        Public Property Price As Decimal
+
+        Public Property Quantity As Integer
+    End Class
 End Namespace
